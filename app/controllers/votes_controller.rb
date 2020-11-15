cf7cec57900ce72ec2af9d6d67bef29aa214d653
EC2 Default User <ec2-user@ip-172-31-49-128.ec2.internal>
2020-11-15 15:05:52 +0000

    vote new index

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -7,6 +7,14 @@ class VotesController < ApplicationController
   end
   
   def create
+    @vote = Vote.new(vote_params)
+    @user = current_user
+    @vote.user_id = current_user.id
+    if @vote.save
+      redirect_to complete_path(@book)#, notice: "You have created vote successfully."
+    else
+      render 'new'
+    end
   end
   
   def index
